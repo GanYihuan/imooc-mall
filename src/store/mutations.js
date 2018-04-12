c7d9f20bc23daa42fb909f41f7076440f48cb79a
GanYihuan <583520052@qq.com>
2018-04-12 09:15:09 +0800

    vuex rewrite

---
+++ src/store/mutations.js
@@ -0,0 +1,12 @@
+import * as types from './mutation-types'
+
+const mutations = {
+  [types.SET_NICKNAME] (state, nickName) {
+    state.nickName = nickName
+  },
+  [types.SET_CART_COUNT] (state, cartCount) {
+    state.cartCount = cartCount
+  }
+}
+
+export default mutations
