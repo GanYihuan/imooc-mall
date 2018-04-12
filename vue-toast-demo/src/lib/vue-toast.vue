112ec97c4700843692cb2376ffd4855c648f76c2
GanYihuan <583520052@qq.com>
2018-04-12 08:04:45 +0800

    add toat.js

---
+++ vue-toast-demo/src/lib/vue-toast.vue
@@ -0,0 +1,41 @@
+<template>
+  <div>
+    <header-component>
+      <div>this is template body</div>
+    </header-component>
+  </div>
+</template>
+
+<script>
+  export default {
+    data () {
+      return {
+        message: 'hello'
+      }
+    }
+  }
+</script>
+
+<style scoped lang="scss" rel="stylesheet/scss">
+  .toast-container {
+    position: absolute;
+    left: 0;
+    top: 0;
+    bottom: 0;
+    right: 0;
+    z-index: 2000;
+    display: flex;
+    justify-content: center;
+    align-items: center;
+  }
+
+  .toast {
+    width: 180px;
+    height: 60px;
+    line-height: 60;
+    text-align: center;
+    border-radius: 10px;
+    background-color: rgba(0, 0, 0, 0.61);
+    color: #fff;
+  }
+</style>
